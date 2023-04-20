eef032fddcd4fb163dde3576e257580d9d27faed
Name <125875608+13985@users.noreply.github.com>
2023-04-20 18:04:44 +0800

    python(?) hello world

--- code.c
+++ code.c
@@ -1,1 +1,19 @@
-// Your C code here
+#include <stdio.h>
+#define HELLO "hello"
+#define SPACE " "
+#define WORLD "world"
+#define EXCLAMATION_MARK "!"
+#define CHANGE_LINE "\n"
+
+#define print(a) printf(a);
+#define def int
+#define greeting() main(void){
+#define return exit(0);}
+
+def greeting()
+  print(HELLO)
+  print(SPACE)
+  print(WORLD)
+  print(EXCLAMATION_MARK)
+  print(CHANGE_LINE)
+  return
